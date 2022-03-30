de249e303ddc8bfcd04798419f89bfa1f79e48be
Roger Best <rbest@microsoft.com>
2022-03-30 15:10:17 -0700

    Add remove users using csv

---
+++ Lab_Management/PowerShell/BulkOperations/Examples/Remove-Students.ps1
@@ -0,0 +1,27 @@
+[CmdletBinding()]
+param(
+    [parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+    [string]
+    $CsvConfigFile = "BulkBellowsCollegeLabs_Test.csv",
+
+    [parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+    [int]
+    $ThrottleLimit = 100
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+# Make sure the input file does exist
+Write-Host $CsvConfigFile
+if (-not (Test-Path -Path $CsvConfigFile)) {
+    Write-Error "Input CSV File must exist, please choose a valid file location..."
+}
+
+Import-Module ../Az.LabServices.BulkOperations.psm1 -Force
+
+$scriptstartTime = Get-Date
+Write-Host "Executing Bulk Student deletion Script, starting at $scriptstartTime" -ForegroundColor Green
+$CsvConfigFile | Import-LabsCsv | Remove-AzLabUsersBulk
+
+Write-Host "Completed running Bulk student deletion Script, total duration $([math]::Round(((Get-Date) - $scriptstartTime).TotalMinutes, 1)) minutes" -ForegroundColor Green
