6f5d52bb01f7c73451037e4f0d3fad04f2c4fc0c
Roger Best <rbest@microsoft.com>
2022-04-08 10:54:36 -0700

    Update publishall

--- Lab_Management/PowerShell/BulkOperations/Examples/PublishAll.ps1
+++ Lab_Management/PowerShell/BulkOperations/Examples/PublishAll.ps1
@@ -4,15 +4,32 @@ param(
     [string]
     $CsvConfigFile,
 
+    [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+    [string]
+    $CsvOutputFile,
+
+    [parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+    [ValidateNotNullOrEmpty()]
+    [switch] $force,
+
     [parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
     [int]
-    $ThrottleLimit = 5
+    $ThrottleLimit = 10
 )
+
+# Make sure the input file does exist
+if (-not (Test-Path -Path $CsvConfigFile)) {
+    Write-Error "Input CSV File must exist, please choose a valid file location..."
+}
+
+# Make sure the output file doesn't exist
+if ((Test-Path -Path $CsvOutputFile) -and (-not $force.IsPresent)) {
+    Write-Error "Output File cannot already exist, please choose a location to create a new output file..."
+}
 
 $outerScriptstartTime = Get-Date
 Write-Host "Executing Lab Creation Script, starting at $outerScriptstartTime" -ForegroundColor Green
 
-#Import-Module ../../Az.LabServices.psm1 -Force
 Import-Module ../Az.LabServices.BulkOperations.psm1 -Force
 
 $labAccountResults = $CsvConfigFile | 
@@ -22,6 +39,7 @@ $labAccountResults = $CsvConfigFile |
         Publish-AzLabsBulk -ThrottleLimit $ThrottleLimit      # Publish all the labs
 
 # Write out the results
+$labAccountResults | Export-LabsCsv -CsvConfigFile $CsvOutputFile -Force:$force.IsPresent
 $labAccountResults | Select-Object -Property ResourceGroupName, LabPlanName, LabName, LabPlanResult, LabResult, PublishResult | Format-Table
 
 Write-Host "Completed running Bulk Lab Creation script, total duration $(((Get-Date) - $outerScriptstartTime).TotalMinutes) minutes" -ForegroundColor Green
